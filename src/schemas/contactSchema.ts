36edf4206eb90665297fd983abed215ee7b5e32b
zogss <yanlucasbranco@gmail.com>
2023-09-08 20:09:27 -0300

    fix: zod schema required error

--- src/schemas/contactSchema.ts
+++ src/schemas/contactSchema.ts
@@ -3,11 +3,16 @@ import * as z from 'zod'
 export const contactSchema = z.object({
   name: z
     .string({ required_error: 'required_field_error' })
+    .nonempty('required_field_error')
     .min(3, 'name_min_length_error')
     .max(50, 'name_max_length_error'),
-  email: z.string({ required_error: 'required_field_error' }).email('email_field_error'),
+  email: z
+    .string({ required_error: 'required_field_error' })
+    .nonempty('required_field_error')
+    .email('email_field_error'),
   message: z
     .string({ required_error: 'required_field_error' })
+    .nonempty('required_field_error')
     .min(5, 'message_min_length_error')
     .max(500, 'message_max_length_error'),
 })
