75a4ece762b7c65526b1f36cd86f3acbcc65f961
zogss <yanlucasbranco@gmail.com>
2023-09-10 23:05:31 -0300

    chore: improving loader animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -8,28 +8,31 @@ const Loader: React.FC = () => (
     aria-label="Loading"
     className="fixed inset-0 z-[9999] flex items-center justify-center bg-charcoal-black-700 text-black transition-all duration-500 ease-in"
   >
-    <div className="flex w-fit -translate-x-10 items-end">
+    <motion.div layout className="flex w-fit items-end">
       <motion.div
-        initial={{ x: 60 }}
-        animate={{ x: 0 }}
+        layout
         transition={{ duration: 1.5, ease: 'easeInOut', delay: 0.5 }}
         className="z-[1] bg-gradient-to-r from-charcoal-black-700 to-transparent pl-10"
       >
         <StaticImage
           src="../images/yan_icon.png"
           alt="Yan's icon"
+          placeholder="blurred"
           className="h-24 w-24 shrink-0 md:h-32 md:w-32 lg:h-36 lg:w-36"
         />
       </motion.div>
-      <motion.span
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1.5, ease: 'easeInOut', delay: 0.5 }}
-        className="text-end text-3xl text-neutral-100 md:text-5xl lg:text-7xl"
+      <motion.div
+        layout
+        initial={{ opacity: 0, width: 0 }}
+        animate={{ opacity: 1, width: 'auto' }}
+        transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
+        className="overflow-hidden"
       >
-        an Lucas
-      </motion.span>
-    </div>
+        <span className="truncate text-end text-3xl text-neutral-100 md:text-5xl lg:text-7xl">
+          an Lucas
+        </span>
+      </motion.div>
+    </motion.div>
   </div>
 )
 
