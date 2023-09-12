de9bfacf1aa102f434d32bd9c27d2d46e1fd56a7
zogss <yanlucasbranco@gmail.com>
2023-09-12 01:28:29 -0300

    styles: size variable type changed

--- src/components/projects/ProjectsBlock.tsx
+++ src/components/projects/ProjectsBlock.tsx
@@ -9,42 +9,42 @@ const ProjectsBlock: React.FC = () => (
       <StaticImage
         src="../../images/projects/spacie_image.png"
         alt={projects[0].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
     <ProjectCard index={1} {...projects[1]}>
       <StaticImage
         src="../../images/projects/cs_analytics_image.png"
         alt={projects[1].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
     <ProjectCard index={2} {...projects[2]}>
       <StaticImage
         src="../../images/projects/expert_stats_image.png"
         alt={projects[2].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
     <ProjectCard index={3} {...projects[3]}>
       <StaticImage
         src="../../images/projects/chirp_image.png"
         alt={projects[3].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
     <ProjectCard index={4} {...projects[4]}>
       <StaticImage
         src="../../images/projects/massagueirinha_image.png"
         alt={projects[4].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
     <ProjectCard index={5} {...projects[5]}>
       <StaticImage
         src="../../images/projects/bullblockchain_image.gif"
         alt={projects[5].alt}
-        className="aspect-[16/9] max-h-[200px] w-full rounded-lg lg:h-[200px] lg:w-[400px]"
+        className="aspect-[16/9] max-h-[12.5rem] w-full rounded-lg lg:h-[12.5rem] lg:w-[25rem]"
       />
     </ProjectCard>
   </div>
