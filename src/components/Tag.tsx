ffb997b518e6b87573c2a43aeb7db4a7ce3a6e0d
zogss <yanlucasbranco@gmail.com>
2023-09-12 04:32:31 -0300

    styles: adapting for mobile screens

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,9 +5,9 @@ interface TagProps {
 }
 
 const Tag: React.FC<TagProps> = ({ text }) => (
-  <div className="flex items-center justify-center rounded-full bg-gradient-primary p-[.125rem]">
-    <div className="flex items-center justify-center rounded-full bg-charcoal-black-700 bg-gradient-secondary px-3.5 py-1.5 shadow-primary backdrop-blur-[1.25rem]">
-      <span className="text-sm font-semibold leading-tight text-white/40">{text}</span>
+  <div className="flex items-center justify-center rounded-full bg-gradient-primary p-0.5">
+    <div className="flex items-center justify-center rounded-full bg-charcoal-black-700 bg-gradient-secondary px-3 py-1 shadow-primary backdrop-blur-[1.25rem] md:px-3.5 md:py-1.5">
+      <span className="text-xs font-semibold leading-tight text-white/40 md:text-sm">{text}</span>
     </div>
   </div>
 )
