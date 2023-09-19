27ff73d9d0f016001b5bb09ffe5fdc43fe025262
zogss <yanlucasbranco@gmail.com>
2023-09-19 02:55:17 -0300

    feat: add I18n types

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,3 +4,36 @@ declare module 'gatsby-plugin-firebase-v9.0' {
   const app: FirebaseApp
   export default app
 }
+
+declare interface I18nPageData {
+  locales: I18nLocales
+}
+
+declare interface I18nLocales {
+  edges: I18nEdge[]
+}
+
+declare interface I18nEdge {
+  node: I18nNode
+}
+
+declare interface I18nNode {
+  ns: string
+  data: string
+  language: string
+}
+
+declare interface I18nPageContext {
+  language: string
+  i18n: I18nPageContextData
+}
+
+declare interface I18nPageContextData {
+  language: string
+  languages: string[]
+  defaultLanguage: string
+  generateDefaultLanguagePage: boolean
+  routed: boolean
+  originalPath: string
+  path: string
+}
