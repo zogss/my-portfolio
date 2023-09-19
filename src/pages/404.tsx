fa0e241e4b04f705bf2d3e6791ee183c0b87a0c0
zogss <yanlucasbranco@gmail.com>
2023-09-19 03:00:33 -0300

    fix: useless code removed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -11,14 +11,6 @@ const NotFoundPage: React.FC<PageProps> = () => (
       <p className="">
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
       </p>
     </div>
     <Link to="/">Go home</Link>
