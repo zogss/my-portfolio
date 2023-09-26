34d0f912d3905eab5613ce0ea1df0d1aae99af0a
zogss <yanlucasbranco@gmail.com>
2023-09-26 04:07:13 -0300

    chore: replacing old tags for new variants

--- src/components/projects/project/ProjectBlock.tsx
+++ src/components/projects/project/ProjectBlock.tsx
@@ -1,10 +1,14 @@
-import clsx from 'clsx'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { useI18next } from 'gatsby-plugin-react-i18next'
-import React, { useMemo } from 'react'
+import { kebabCase } from 'lodash'
+import React from 'react'
 import { BiLinkExternal } from 'react-icons/bi'
 import { BsGithub } from 'react-icons/bs'
-import { ProjectType, getBgFromProject } from '~/utils'
+import Tag from '~/components/Tag'
+import ProjectSeparator from '~/components/variants/projectSeparator'
+import ProjectTitleText from '~/components/variants/projectTitleText'
+import ProjectTripleDots from '~/components/variants/projectTripleDots'
+import { ProjectType } from '~/utils'
 
 const ProjectBlock: React.FC<ProjectType> = ({
   title,
@@ -13,18 +17,16 @@ const ProjectBlock: React.FC<ProjectType> = ({
   image,
   alt,
   slug,
+  techs,
   url,
   repository_url,
 }) => {
   //* hooks
   const { t } = useI18next()
 
-  //* memos
-  const projectBackground = useMemo(() => getBgFromProject(slug), [slug])
-
   //* render
   return (
-    <section className="flex w-full flex-col gap-4">
+    <section className="flex w-full flex-col gap-9">
       <div className="flex w-full flex-col items-start gap-3 md:gap-4">
         <GatsbyImage
           image={getImage(image)!}
@@ -32,6 +34,19 @@ const ProjectBlock: React.FC<ProjectType> = ({
           className="h-full w-full rounded-md object-contain object-center"
         />
         <div className="flex w-full items-center justify-between">
+          <div className="group/projectCard flex items-center justify-start gap-4">
+            <ProjectTitleText
+              as="a"
+              href={url || 'javascript:void(0)'}
+              target={url ? '_blank' : undefined}
+              rel={url ? 'noopener noreferrer' : undefined}
+              title={url || undefined}
+              color={slug}
+            >
+              {t(title)}
+            </ProjectTitleText>
+            <ProjectTripleDots color={slug} size="md" />
+          </div>
           {(url || repository_url) && (
             <div className="flex items-center justify-start gap-1 md:gap-2">
               {url && (
@@ -40,9 +55,9 @@ const ProjectBlock: React.FC<ProjectType> = ({
                   target="_blank"
                   rel="noopener noreferrer"
                   title={url}
-                  className="flex w-full items-center justify-center rounded-md p-1 text-sm font-semibold transition-colors duration-500 hover:bg-white/5 hover:text-white focus:outline-none md:gap-2 md:px-3.5 md:py-2 lg:gap-2.5 lg:px-4 lg:py-2.5"
+                  className="flex w-full items-center justify-center rounded-md p-1 text-sm font-semibold transition-colors duration-500 hover:bg-white/10 hover:text-white focus:outline-none md:gap-2 md:p-2 lg:gap-2.5 lg:p-2.5"
                 >
-                  <span className="sr-only md:not-sr-only">{t('visit')}</span>
+                  <span className="sr-only">{t('visit')}</span>
                   <BiLinkExternal className="h-5 w-5 shrink-0 md:h-6 md:w-6" />
                 </a>
               )}
@@ -52,31 +67,34 @@ const ProjectBlock: React.FC<ProjectType> = ({
                   target="_blank"
                   rel="noopener noreferrer"
                   title={repository_url}
-                  className="flex w-full items-center justify-center rounded-md p-1 text-sm font-semibold transition-colors duration-500 hover:bg-white/5 hover:text-white focus:outline-none md:gap-2 md:px-3.5 md:py-2 lg:gap-2.5 lg:px-4 lg:py-2.5"
+                  className="flex w-full items-center justify-center rounded-md p-1 text-sm font-semibold transition-colors duration-500 hover:bg-white/10 hover:text-white focus:outline-none md:gap-2 md:p-2 lg:gap-2.5 lg:p-2.5"
                 >
-                  <span className="sr-only md:not-sr-only">{t('code')}</span>
+                  <span className="sr-only">{t('code')}</span>
                   <BsGithub className="w-h-5 md:6 shmd:rink-0 h-6 w-5" />
                 </a>
               )}
             </div>
           )}
-          <div className="flex items-start justify-center gap-1.5">
-            {Array.from({ length: 3 }).map((_, i) => (
-              <div
-                key={`${title}-circle-${i}`}
-                className={clsx('h-2 w-2 rounded-full lg:h-3 lg:w-3', projectBackground)}
-              />
-            ))}
-          </div>
         </div>
       </div>
       <div className="w-full text-sm md:text-base">
-        <h6>{t(short_description)}</h6>
+        <span>{t(short_description)}</span>
         {long_description.map((paragraph, i) => (
           <p key={`${title}-paragraph-${i}`} className="mt-3">
             {t(paragraph)}
           </p>
         ))}
+      </div>
+      <div className="mt-5 flex w-full flex-col items-start gap-3">
+        <div className="flex w-full items-end justify-start gap-2">
+          <span className="text-sm font-semibold text-neutral-400">{t('techs')}</span>
+          <ProjectSeparator color={slug} size="xs" />
+        </div>
+        <div className="flex w-full flex-wrap items-center justify-center gap-1.5">
+          {techs.map((tech, i) => (
+            <Tag key={`${kebabCase(title)}-${tech}-${i}`} text={tech} />
+          ))}
+        </div>
       </div>
     </section>
   )
--- src/components/projects/project/ProjectCard.tsx
+++ src/components/projects/project/ProjectCard.tsx
@@ -1,15 +1,12 @@
-import clsx from 'clsx'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Link, useI18next } from 'gatsby-plugin-react-i18next'
-import React, { useMemo } from 'react'
-import { ProjectType, getBgFromProject } from '~/utils'
+import React from 'react'
+import ProjectTripleDots from '~/components/variants/projectTripleDots'
+import { ProjectType } from '~/utils'
 
 const ProjectCard: React.FC<ProjectType> = ({ image, alt, slug, title }) => {
   //* hooks
   const { t } = useI18next()
-
-  //* memos
-  const projectBackground = useMemo(() => getBgFromProject(slug), [slug])
 
   //* render
   return (
@@ -34,18 +31,7 @@ const ProjectCard: React.FC<ProjectType> = ({ image, alt, slug, title }) => {
               </div>
             </div>
             <header className="flex w-full items-center justify-center gap-3 lg:gap-5 xl:w-fit">
-              <div className="flex items-start justify-center gap-1.5">
-                {Array.from({ length: 3 }).map((_, i) => (
-                  <div
-                    key={`${title}-circle-${i}`}
-                    className={clsx('h-2 w-2 rounded-full', projectBackground, {
-                      'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out]': i === 0,
-                      'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out_0.1s]': i === 1,
-                      'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out_0.2s]': i === 2,
-                    })}
-                  />
-                ))}
-              </div>
+              <ProjectTripleDots color={slug} size="sm" />
               <h3 className="text-lg font-semibold text-neutral-300 lg:pr-4 lg:text-xl xl:pr-0">
                 {t(title)}
               </h3>
@@ -58,127 +44,3 @@ const ProjectCard: React.FC<ProjectType> = ({ image, alt, slug, title }) => {
 }
 
 export default ProjectCard
-
-/* 
-<div data-animation="animate" className="flex grow flex-col">
-      <div
-        role={isSmaller ? 'button' : undefined}
-        tabIndex={isSmaller ? 0 : undefined}
-        onClick={isSmaller ? () => navigate(`projects/${slug}`) : undefined}
-        onKeyDown={isSmaller ? (e) => e.key === 'Enter' && navigate(`projects/${slug}`) : undefined}
-        data-animation-target={index % 2 !== 0 ? 'left' : 'right'}
-        className="flex flex-col items-center gap-3 lg:w-full"
-      >
-        <div
-          className={clsx(
-            'group/projectCard relative flex flex-col items-start gap-3 self-stretch overflow-hidden rounded-lg bg-midnight-slate-700 p-4 md:p-6',
-            {
-              'transition-colors hover:bg-zinc-700': isSmaller,
-            }
-          )}
-        >
-          <div
-            className={clsx(
-              'z-[1] flex flex-col items-center gap-4 self-stretch md:gap-6 xl:items-start'
-              // index % 2 !== 0 ? 'xl:flex-row-reverse' : 'xl:flex-row'
-            )}
-          >
-            <div className="group/projectImg max-h-[12.5rem] shrink-0 overflow-hidden rounded sm:h-[12.5rem] sm:max-h-max 2xl:h-[13.75rem]">
-              <GatsbyImage
-                image={getImage(image)!}
-                alt={alt}
-                className="aspect-[18/9] max-h-[12.5rem] w-full transition-all duration-[5000ms] ease-[cubic-bezier(0.7,0,0.7,1.01)] hover:scale-125 sm:w-auto 2xl:max-h-[13.75rem]"
-              />
-            </div>
-            <div
-              className={clsx(
-                'z-10 flex flex-1 flex-col items-center gap-2.5',
-                index % 2 !== 0 ? 'flex-row-reverse md:items-end' : 'md:items-start'
-              )}
-            >
-              <div
-                className={clsx('flex w-full items-center justify-center gap-4 lg:gap-6 xl:w-fit', {
-                  'xl:flex-row-reverse': index % 2 !== 0,
-                })}
-              >
-                <div className="flex items-start justify-center gap-1.5">
-                  {Array.from({ length: 3 }).map((_, i) => (
-                    <div
-                      key={`${title}-circle-${i}`}
-                      className={clsx('h-2 w-2 rounded-full', projectBackground, {
-                        'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out]': i === 0,
-                        'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out_0.1s]': i === 1,
-                        'group-hover/projectCard:animate-[wiggleUp_0.5s_ease-in-out_0.2s]': i === 2,
-                      })}
-                    />
-                  ))}
-                </div>
-                <h3 className="text-xl font-semibold lg:pr-5 lg:text-2xl xl:pr-0">{t(title)}</h3>
-              </div>
-              {isSmaller && (
-                <p className="text-justify indent-5 text-base font-medium text-white/30">
-                  {t(description)}
-                </p>
-              )}
-            </div>
-          </div>
-          {isSmaller && (
-            <>
-              {(url || repository_url) && (
-                <div
-                  className={clsx(
-                    'z-[1] flex w-full items-center justify-center gap-3',
-                    index % 2 !== 0 ? 'xl:justify-end' : 'xl:justify-start'
-                  )}
-                >
-                  {url && (
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      title={url}
-                      className={clsx(
-                        'flex w-full items-center justify-center gap-2.5 rounded-md bg-transparent px-4 py-2.5 text-sm font-semibold transition-colors duration-500 focus:outline-none md:w-fit',
-                        projectBackground
-                      )}
-                    >
-                      <span>{t('visit')}</span>
-                      <BiLinkExternal className="h-6 w-6 shrink-0" />
-                    </a>
-                  )}
-                  {repository_url && (
-                    <a
-                      href={repository_url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      title={repository_url}
-                      className={clsx(
-                        'flex w-full items-center justify-center gap-2.5 rounded-md bg-transparent px-4 py-2.5 text-sm font-semibold transition-colors duration-500 focus:outline-none md:w-fit',
-                        projectBackground
-                      )}
-                    >
-                      <span>{t('code')}</span>
-                      <BsGithub className="h-6 w-6 shrink-0" />
-                    </a>
-                  )}
-                </div>
-              )}
-              <LinesUnion
-                className={clsx(
-                  'absolute -top-1/3',
-                  index % 2 === 0 ? '-right-1/4 xl:-top-2/3' : '-left-1/4 -scale-x-[1] xl:-top-2/3'
-                )}
-              />
-            </>
-          )}
-        </div>
-        {isSmaller && (
-          <div className="flex w-full flex-wrap items-center justify-center gap-1.5">
-            {techs.map((tech, i) => (
-              <Tag key={`${kebabCase(title)}-${tech}-${i}`} text={tech} />
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-*/
