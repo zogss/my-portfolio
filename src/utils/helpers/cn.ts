2620e3c80bd0f8cda45b411dcb922ec323a5e6e1
zogss <yanlucasbranco@gmail.com>
2025-01-04 21:56:06 -0300

    refactor: update cn utility function to use clsx for class merging

--- src/utils/helpers/cn.ts
+++ src/utils/helpers/cn.ts
@@ -1,17 +1,16 @@
+import {clsx, type ClassValue} from 'clsx';
 import {twMerge} from 'tailwind-merge';
 
 /**
  * @description
  * A utility function to merge tailwind classes.
  * used for conditional merging of tailwind classes.
- * @param classNames - An array of tailwind classes
+ * @param inputs - An array of tailwind classes
  * @returns A string of tailwind classes.
  * @example
  * cn('text-red-500', 'text-red-600') => text-red-600
  * cn('text-red-500', undefined, 'bg-red-500')
  * **/
-export const cn = (
-  ...classNames: Array<string | undefined | string>
-): string => {
-  return twMerge(classNames);
+export const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(clsx(inputs));
 };
