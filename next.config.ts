186cdd52bff21a82b4ec6005b5c0cecbeca8af29
zogss <yanlucasbranco@gmail.com>
2025-05-05 16:16:01 -0300

    chore: remove unused image configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        hostname: '**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
