6a48e2ff95383a843b0ed73f92de2f6648fcb478
zogss <yanlucasbranco@gmail.com>
2026-04-29 18:33:34 -0300

    chore: add .next directory to ESLint ignore list and configure image formats and qualities in Next.js settings

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       'static',
       '.gitignore',
       '.cache',
+      '.next',
     ],
   },
   {
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    qualities: [85, 75],
+  },
 };
 
 export default nextConfig;
